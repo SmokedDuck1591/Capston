d06142d02a9e6299a3b8cae6756d82ae4a3b7f09
SmokedDuck1591 <70051389+SmokedDuck1591@users.noreply.github.com>
2020-09-23 22:20:25 +0900

    Category Inline ver & popup with cancel button

--- createPage.js
+++ createPage.js
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import QuillEditor from "../../libs/QuillEditor";
+import Tags from "../../libs/EditableTagGroup";
 import Header from "../../libs/Header/Header";
-import { DownOutlined } from "@ant-design/icons";
-import { Typography, Input, Select, Divider, Button, Checkbox } from "antd";
+import { Typography, Input, Select, Divider, Button, Checkbox, Popconfirm } from "antd";
 import "react-quill/dist/quill.snow.css";
 import "./createPage.css";
+import { withRouter } from 'react-router-dom';
+
 
 function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+function tagHandleChange(value) {
   console.log(`selected ${value}`);
 }
 function onChange(e) {
@@ -16,6 +21,11 @@ function onChange(e) {
 const { Title, Text } = Typography;
 const children = [];
 const { Option } = Select;
+const text = 'Are you sure to delete this post?';
+
+const goBack = () => {
+  this.props.history.goBack();
+}
 
 /*
 const categoryMenu = () => {
@@ -34,7 +44,7 @@ const CreatePost = () => {
     <section className="write__container">
       <div className="container__title">
         <Title level={3}>Title</Title>
-        <Input placeholder="Add a Title" />
+        <Input placeholder="Add a Title" className="title__Input" />
         <Select defaultValue="카테고리를 선택해 주세요." onChange={handleChange}>
           <Option value="1">자취방 개꿀팁</Option>
           <Option value="2">내가 요리왕이다</Option>
@@ -44,20 +54,18 @@ const CreatePost = () => {
       <div className="container__tags">
         <Divider />
         <Title level={3}>Tags</Title>
-        <Select mode="tags" placeholder="Add Tags" className="tags__tag">
-          {children}
-        </Select>
+        <Tags />
       </div>
       <div className="container__Description">
         <Divider />
         <Title level={3}>Description</Title>
         <div className = "container__editor">
-        <QuillEditor placeholder={"Start Posting Something"} />
+        <QuillEditor placeholder={"Start Posting Something"} className="description__Editor"/>
         </div>
       </div>
       <div className="container__Setting">
         <Divider />
-        <Title Title level={3}>
+        <Title level={3}>
           Settings
         </Title>
         <Checkbox onChange={onChange}>Close comments</Checkbox>
@@ -66,10 +74,12 @@ const CreatePost = () => {
       <div className="container__Button">
         <Divider />
         <Button type="dashed">Update</Button>
+        <Popconfirm placement="right" title={text} onConfirm={()=> {}}>
         <Button danger>Cancel</Button>
+        </Popconfirm>
       </div>
     </section>
   );
 };
 
-export default CreatePost;
+export default withRouter(CreatePost);
